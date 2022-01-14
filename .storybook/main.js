326220c6e93ed08a0021c8a56716bde4061ea473
Bodo Graumann <mail@bodograumann.de>
2022-01-14 16:21:56 +0100

    Install interactions-addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,10 +5,22 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
   ],
   "framework": "@storybook/vue3",
   core: {
     builder: "storybook-builder-vite"
+  },
+  async viteFinal(config, { configType }) {
+    // https://github.com/eirslett/storybook-builder-vite/issues/186#issuecomment-1001064670
+    if (configType !== "PRODUCTION") {
+      config.optimizeDeps ??= { include: [] };
+      config.optimizeDeps.include.push(
+        // Fix: `@storybook/addon-interactions` exports is not defined or `jest-mock` does not provide an export named "fn"
+        "jest-mock",
+      );
+    }
+    return config;
   }
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import * as jest from "@storybook/jest";
+window.jest = jest;
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
